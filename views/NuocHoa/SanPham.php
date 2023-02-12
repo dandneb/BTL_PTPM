cee33c4c7d248a25229782b3c10feae5c06972fb
Đán Đào <daodan2612@gmail.com>
2023-02-13 05:22:15 +0700

    5:22 AM 13/2/2023

--- views/NuocHoa/SanPham.php
+++ views/NuocHoa/SanPham.php
@@ -13,7 +13,7 @@ require("views/template/header.php");
             </nav>
         </div>
     </div>
-    <div class="container">
+    <div class="container pb-3">
         <div class="row">
             <div class="section-sp col-md-3 me-3 border p-0">
                 <div class="accordion" id="accordionPanelsStayOpenExample" style="padding: 10px;">
@@ -157,7 +157,7 @@ require("views/template/header.php");
             <div class="col-md-3 me-3">
 
             </div>
-            <div class="col-md-8 border">
+            <div class="col-md-8 border mo_ta">
                 <div style="padding: 10px;">
                     <div class="paragraph-sp">
                         <p class="p-14">Nước hoa nam là sản phẩm nước hoa/dầu thơm dành cho nam giúp thể hiện phong cách, cá tính và mang lại một lợi thế giúp thu hút người đối diện hoặc những người xung quanh. Để chọn nước hoa phù hợp với bản thân thì có nhiều cách từ việc phù hợp với lứa tuổi, theo phong cách, theo mùa,… Thì việc xịt nước hoa cũng giúp cho nước hoa Nam thể hiện hết “sức mạnh” của chúng.</p>
@@ -170,6 +170,13 @@ require("views/template/header.php");
                         <p class="p-14">
                             Bên cạnh trang phục thì nước hoa cũng giúp cho người đối diện nhớ đến mùi thơm của bạn. Chọn loại nước hoa nam phù hợp cũng giúp cho bạn tạo cá tính riêng cũng như thể hiện được bản thân, và ấn tượng riêng đến với người xung quanh.
                         </p>
+                        <div class="container-fluid btn-xemthem">
+                            <div class="button-sp text-center">
+                                <span class="btn btn-xt">XEM THÊM <span class="material-icons" style="transform:translateY(6px)">
+                                        expand_more
+                                    </span></span>
+                            </div>
+                        </div>
                         <h4>
                             Mang đến sự tự tin cho nam giới
                         </h4>
@@ -238,37 +245,104 @@ require("views/template/header.php");
                             Cách chọn nước hoa cho Nam theo loại sản phẩm
                         </h3>
                         <h4>
-
+                            Chọn nước hoa theo thời gian lưu hương
                         </h4>
                         <p class="p-14">
-
+                            Bạn có thể thấy được độ lưu hương của nước hoa trên bao bì của sản phẩm. Các thông số bạn có thể nhìn vào:
+                        </p>
+                        <p class="p-14">
+                            <span class="p-14-bold">Eau Fraiche: </span>
+                            Là loại loãng nhất hiện nay chỉ có 1-3% tinh dầu, mùi hương lưu lại trong vòng 1-2 giờ.
+                        </p>
+                        <p class="p-14">
+                            <span class="p-14-bold">Eau de cologne: </span>
+                            Có độ lưu hương thấp nhất khoảng 2 giờ, chứa khoảng 3-5% tinh dầu trong hỗn hợp cồn và nước.
+                        </p>
+                        <p class="p-14">
+                            <span class="p-14-bold">Eau de Toilette: </span>
+                            Là loại nước hoa có độ lưu hương 3-5 giờ, chứa 5-8% dầu tinh chất.
+                        </p>
+                        <p class="p-14">
+                            <span class="p-14-bold">Eau de Parfum: </span>
+                            Với hàm lượng tinh dầu tinh chất 16-20%, mùi hương sẽ lưu lại từ 6-8 tiếng.
+                        </p>
+                        <p class="p-14">
+                            <span class="p-14-bold">Parfum Classic: </span>
+                            Là loại nước hoa đậm đặc nhất có thể lưu hương cả ngày, chứa khoảng 22-40% tinh dầu nguyên chất.
                         </p>
                         <h4>
-
+                            Chọn nước hoa theo mùi hương:
                         </h4>
                         <p class="p-14">
-
+                            Về cơ bản, nước hoa nam có những nhóm mùi sau đây:
+                        </p>
+                        <p class="p-14">
+                            <span class="p-14-bold">Aromatic (hương thơm mát): </span>
+                            Đây là loại hương thơm của các loại cây thảo mộc như cây xô thơm, hương thảo, thì là, hoa oải hương và một số loài cây có sở hữu mùi cỏ thảo mộc nồng nàn.
+                        </p>
+                        <p class="p-14">
+                            <span class="p-14-bold">Citrus (hương Cam chanh): </span>
+                            Chỉ cần nghe tên cũng biết được đây là mùi thơm của các loại cây họ chanh, cam như hương chanh, cam, cam bergamot, bưởi chùm hoặc quýt,... pha lẫn với các hương cam chanh trong nước hoa của nam giới.
+                        </p>
+                        <p class="p-14">
+                            <span class="p-14-bold">Floral (hương hoa cỏ): </span>
+                            Nhóm hương này thường có sự góp mặt của hoa nhài, mẫu đơn, dành dành, huệ, linh lan, mộc lan, trinh nữ,….
+                        </p>
+                        <p class="p-14">
+                            <span class="p-14-bold">Woody (hương gỗ): </span>
+                            Đây là nhóm các mùi hương từ gỗ như gỗ đàn hương, tuyết tùng, trầm hương, gỗ guaiac,...
+                        </p>
+                        <p class="p-14">
+                            <span class="p-14-bold">Leather (hương da thuộc): </span>
+                            Nhóm hương da thuộc mang nhiều cung bậc âm hưởng từ mang hương hoa cỏ mượt mà đến mang hương vị chua, có mùi khói.
                         </p>
                         <h4>
-
+                            Cách chọn nước hoa cho Nam theo mùa
                         </h4>
                         <p class="p-14">
-
+                            <span class="p-14-bold">Mùa Hè: </span>
+                            Với mùa nóng bạn nên chọn những loại nước hoa có mùi hương dịu nhẹ và sảng khoái. Giúp cho bạn và người xung quanh có cảm giác dễ chịu. Mùa này, một số mùi hương như: hương hoa hoắc hương mùi cam chanh sẽ khá phù hợp để bạn nam sử dụng vào mùa hè
+                        </p>
+                        <p class="p-14">
+                            <span class="p-14-bold">Mùa thu: </span>
+                            Đây là thời điểm tiết trời mát mẻ, những dòng nước hoa chuyển tiếp từ hè sang thu hoặc có mùi nồng hơn là sự lựa chọn hoàn hảo. Hương quế là sự lựa chọn hoàn hảo tới mùa thu.
+                        </p>
+                        <p class="p-14">
+                            <span class="p-14-bold">Mùa đông: </span>
+                            Mùa đông với tiết trời mát mẻ thì việc chọn mùi hương cũng khác với mùi hè. Ở mùa đông thì nước hoa sẽ lưu hương lâu hơn. Những mùi như mùi xạ hương hay mùi Spicy là một sự lựa chọn hoàn hảo nhất trong thời tiết lạnh.
+                        </p>
+                        <p class="p-14">
+                            <span class="p-14-bold">Mùa xuân: </span>
+                            Mùa xuất là mùi của cỏ cây hoa lá bắt đầu phát triển. Vì vậy,các bạn nam hãy chọn cho mình một mùi nước hoa nhẹ nhàng thuộc dạng mùi cam chanh tươi mát, đầy sức sống để có thể diễn tả lại sự ấm áp, dễ chịu của khí trời mùa xuân.
                         </p>
                         <h4>
-
+                            Cách xịt nước hoa nam đúng cách
                         </h4>
                         <p class="p-14">
-
+                            Chọn mùi nước hoa là một chuyện, sử dụng nước hoa nam đúng cách cũng là vấn đề bạn cần quan tâm. Để sử dụng phù hợp không nên sử dụng quá nhiều làm cho mùi nước hoa quá nồng. Hãy xịt vừa đủ và ở những vị trí chính xác sẽ giúp nước hoa lưu hương cũng như lan tỏa tốt hơn.
+                        </p>
+                        <p class="p-14">
+                            Có hai vị trí xịt nước hoa nam giới cần ghi nhớ là cổ họng và vùng ngực. Tại đây nước hoa sẽ hòa quyện vào mùi của cơ thể bạn tạo ra một mùi hương đặc trưng dành cho chính bạn.
+                        </p>
+                        <p class="p-14">
+                            Bạn chỉ nên xịt vào tai dành cho va chạm gần mà thôi. Bạn cũng có thể xịt nước hoa vào hai cổ tay để những mạch đập nơi cổ tay giúp bạn phân tán nước hoa đi khắp cơ thể.
                         </p>
                         <h4>
-
+                            Địa chỉ mua nước hoa Nam chiết chính hãng
                         </h4>
                         <p class="p-14">
-
+                            Ngày nay, nước hoa Nam là một thứ vũ khí vô hình giúp tăng sức quyến rủ cũng như lôi cuốn của bản thân. Việc lựa chọn và mua nước hoa nam chính hãng là điều cần thiết, bởi nước hoa hàng hiệu cho nam có giá không rẻ, vì vậy lựa chọn nước hoa/ dầu thơm nam chính hãng tại Parfumerie giúp bạn yên tâm hơn. Bởi chúng tôi cam kết tất cả sản phẩm bán ra đều là sản phẩm nước hoa chính hãng.
                         </p>
-                    
-                    
+                        <p class="p-14">
+                            Tại cửa hàng nước hoa Parfumerie - Chúng tôi cung cấp các sản phẩm nước hoa nam từ fullbox, nguyên seal đến các loại nước hoa chiết cho nam với giá cả phù hợp cũng như chất lượng chính hãng.
+                        </p>
+                        <div class="container-fluid btn-rutgon">
+                            <div class="text-center">
+                                <span class="btn btn-rg">THU GỌN <span class="material-icons" style="transform:translateY(6px)">
+                                    arrow_upward
+                                    </span></span>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -278,24 +352,39 @@ require("views/template/header.php");
 <script>
     $(window).resize(function() {
         if (window.matchMedia('(max-width: 1200px)').matches) {
-            $( ".col-md-3" ).removeClass( "col-md-3" ).addClass( "col-md-6" );
-            $( ".list-product-sp" ).removeClass( "col-md-8" ).addClass( "col-md-12" );
-            //list-product-sp
-            $( ".section-sp" ).removeClass( "col-md-3" ).addClass( "col-md-12" );
+            $(".col-md-3").removeClass("col-md-3").addClass("col-md-6");
+            $(".list-product-sp").removeClass("col-md-8").addClass("col-md-12");
+            $(".mo_ta").removeClass("col-md-8").addClass("col-md-12");
+            $(".section-sp").removeClass("col-md-3").addClass("col-md-12");
+
         }
         if (window.matchMedia('(min-width: 1200px)').matches) {
-            $( ".col-md-6" ).removeClass( "col-md-6" ).addClass( "col-md-3" );
-            $( ".list-product-sp" ).removeClass( "col-md-12" ).addClass( "col-md-8" );
-            $( ".section-sp" ).removeClass( "col-md-12" ).addClass( "col-md-3" );
+            $(".col-md-6").removeClass("col-md-6").addClass("col-md-3");
+            $(".list-product-sp").removeClass("col-md-12").addClass("col-md-8");
+            $(".mo_ta").removeClass("col-md-12").addClass("col-md-8");
+            $(".section-sp").removeClass("col-md-12").addClass("col-md-3");
         }
     });
-    $(".accordion-header").click(function(){
-        if($(".section-sp").css("max-height")=="600px"){
+    $(".accordion-header").click(function() {
+        if ($(".section-sp").css("max-height") == "600px") {
             $(".section-sp").css("max-height", "105px");
-        } 
-        else{
+        } else {
             $(".section-sp").css("max-height", "600px");
         }
+    });
+    $(document).ready(function() {
+        $(window).on('load', function() {
+            $(".btn-xt").click(function() {
+                $(".paragraph-sp").css("height", "100%");
+                $(".btn-xemthem").css("display", "none");
+                $(".btn-rutgon").css("display", "block");
+            });
+            $(".btn-rg").click(function() {
+                $(".paragraph-sp").css("height", "350px");
+                $(".btn-xemthem").css("display", "block");
+                $(".btn-rutgon").css("display", "none");
+            });
+        });
     });
 </script>
 <?php
